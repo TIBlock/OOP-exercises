04bf43d8b70cf4b7f4da4ea942bb78df2c044141
Taylor Blocker <taylorblocker@gmail.com>
2019-11-16 12:26:22 -0600

    comitting minor changes before tackling the hand constructor portion of the app

--- card.js
+++ card.js
@@ -1,16 +1,25 @@
 class Card {
-    constructor(point, suit) {
+    constructor(point, suit, value) {
         this.point = point;
         this.suit = suit;
+        this.value = value;
     }
-    getImageUrl(){
-        console.log('images/' + this.point + "_of_" + this.suit + ".png" )
+    getImageUrl() {
+        console.log("images/" + this.point + "_of_" + this.suit + ".png");
     }
 }
 
-let myCard = new Card(5, "diamonds");
+let myCard = new Card(5, "diamonds", 5);
 
 console.log(myCard.point);
 console.log(myCard.suit);
+console.log(myCard.value);
 
-myCard.getImageUrl()
+myCard.getImageUrl();
+ 
+
+// class Hand {
+//     constructor()
+// }
+
+var myHand;
